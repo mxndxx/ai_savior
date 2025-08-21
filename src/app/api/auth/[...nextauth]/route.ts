fc31c4e7792b9379bb838d532e597bba94812bc9
rayansekkatkr <rayan.sekkat@gmail.com>
2025-08-22 01:43:31 +0900

    repeir login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,8 +5,8 @@ import { createClient } from "@supabase/supabase-js";
 import bcrypt from "bcryptjs";
 
 function getDbAdmin() {
-  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+  const url = process.env.SUPABASE_URL!;
+  const key = process.env.SUPABASE_SERVICE_ROLE_KEY!;
   return createClient(url, key, { auth: { persistSession: false } });
 }
 
